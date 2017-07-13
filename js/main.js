7bee9bb71b5f777681db9cc3db2a9db002d3700f
Administrator <296638406@qq.com>
2017-07-13 20:34:54 +0800

    change images name

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ $(document).ready(function () {
                 $('html,body').animate({
                         scrollTop: targetOffset
                     },
-                    8000);
+                    800);
                 return false;
             }
         }
