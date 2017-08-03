985503ed9e8c3ddc35847aca47003fca80439392
Administrator <296638406@qq.com>
2017-08-03 18:01:04 +0800

    html5shiv and respond.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     $('a[href*=#],area[href*=#]').click(function () {
-        if (location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') && location.hostname === this.hostname) {
+        if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
             var $target = $(this.hash);
             $target = $target.length && $target || $('[name=' + this.hash.slice(1) + ']');
             if ($target.length) {
