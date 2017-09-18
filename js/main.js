a03c373864930f88626a34ed9124ddfc86f7b779
Administrator <296638406@qq.com>
2017-09-18 11:16:28 +0800

    commit

--- js/main.js
+++ js/main.js
@@ -14,6 +14,7 @@ $(document).ready(function () {
         }
     });
 });
+/*
 window.sr = ScrollReveal({reset: true});
 sr.reveal('.learning-title', {delay: 500, origin: 'top', distance: '140px'});
 sr.reveal('.icon-html', {delay: 800});
@@ -24,4 +25,4 @@ sr.reveal('.sr-weibo', {delay: 1000, origin: 'left', distance: '50px'});
 sr.reveal('.sr-facebook', {delay: 900, origin: 'left'});
 sr.reveal('.sr-github', {delay: 800, distance: '0'});
 sr.reveal('.sr-instagram', {delay: 900, origin: 'right'});
-sr.reveal('.sr-linkedin', {delay: 1000, origin: 'right', distance: '50px'});
+sr.reveal('.sr-linkedin', {delay: 1000, origin: 'right', distance: '50px'});*/
