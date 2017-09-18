1e09e5339de8ffe6cbbcaee8e2025161b019cc0b
Administrator <296638406@qq.com>
2017-09-18 17:51:00 +0800

    commit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+//jQuery锚点平滑滚动
 $(document).ready(function () {
     $('a[href*=#],area[href*=#]').click(function () {
         if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
@@ -14,15 +15,4 @@ $(document).ready(function () {
         }
     });
 });
-/*
-window.sr = ScrollReveal({reset: true});
-sr.reveal('.learning-title', {delay: 500, origin: 'top', distance: '140px'});
-sr.reveal('.icon-html', {delay: 800});
-sr.reveal('.icon-css', {delay: 1000});
-sr.reveal('.icon-js', {delay: 1200});
-sr.reveal('.contact-title', {delay: 500});
-sr.reveal('.sr-weibo', {delay: 1000, origin: 'left', distance: '50px'});
-sr.reveal('.sr-facebook', {delay: 900, origin: 'left'});
-sr.reveal('.sr-github', {delay: 800, distance: '0'});
-sr.reveal('.sr-instagram', {delay: 900, origin: 'right'});
-sr.reveal('.sr-linkedin', {delay: 1000, origin: 'right', distance: '50px'});*/
+
