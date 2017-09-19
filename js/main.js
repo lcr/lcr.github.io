d25811c9389b4386ee71cbf062a05d0bd814659f
Administrator <296638406@qq.com>
2017-09-19 15:47:16 +0800

    commit

--- js/main.js
+++ js/main.js
@@ -15,28 +15,3 @@ $(document).ready(function () {
         }
     });
 });
-window.onload = function () {
-    var btn = document.getElementById('backtothetop');
-    // 获取视界高度；
-    var winH = document.documentElement.clientHeight;
-    // 定义计时器；
-    var timer = null;
-    // 定义是否抵达顶部布尔值判断；
-    var isTop = true;
-    // 设置滚动事件；
-    window.onscroll = function () {
-        var toTop = document.body.scrollTop || document.documentElement.scrollTop;
-        // 判断是否到了第二屏，若是，显示按钮；
-        if (toTop >= winH) {
-            btn.style.display = 'block';
-        } else {
-            btn.style.display = 'none';
-        }
-        // 判断是否抵达顶部，若否，停止计时器；
-        if (!isTop) {
-            clearInterval(timer);
-        }
-        // 重置布值判断；
-        isTop = false;
-    };
-};
