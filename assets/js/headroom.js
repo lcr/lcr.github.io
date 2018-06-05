a84c99236db5ab54dfc478186fa34209f7052a6a
Leslie Lai <lcrc@foxmail.com>
2018-06-05 21:12:23 +0800

    commit

--- assets/js/headroom.js
+++ assets/js/headroom.js
@@ -1,5 +1,5 @@
 /*!
- * headroom.min.js v0.9.4 - Give your page some headroom. Hide your header until you need it
+ * headroom.js v0.9.4 - Give your page some headroom. Hide your header until you need it
  * Copyright (c) 2017 Nick Williams - http://wicky.nillia.ms/headroom.js
  * License: MIT
  */
