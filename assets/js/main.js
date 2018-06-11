021985246317a1dc0040440a159ecc3b9008db27
Leslie Lai <lcrc@foxmail.com>
2018-06-11 23:25:54 +0800

    change js flies

--- assets/js/main.js
+++ assets/js/main.js
@@ -120,17 +120,6 @@ backTop.onclick = function () {
   }, 20);
 };
 
-
-/*
- * Archive page Accordion Check
- */
-var urlHash = window.location.hash;
-if (urlHash.length > 0) {
-  var inputCheck = document.getElementById(urlHash);
-  inputCheck.checked = true;
-}
-
-
 /*
  * google analytics
  */
@@ -140,52 +129,3 @@ window.ga = window.ga || function () {
 ga.l = +new Date;
 ga('create', '{{ site.google_analytics }}', 'auto');
 ga('send', 'pageview');
-
-/*
- * Comment Form
- */
-var commentForm = document.querySelector('.comment-form');
-var commentFormInputs = document.querySelectorAll('.comment-form-input');
-var sendFailedDiv = document.querySelector('.comment-form-sendFailed');
-var sendSucceededDiv = document.querySelector('.comment-form-sendSucceeded');
-var overlayDiv = document.querySelector('.comment-form-overlay');
-
-function post(url, data, callback, errorCallback) {
-  var xhr = new XMLHttpRequest();
-  xhr.open('POST', url);
-  xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-  xhr.onload = function () {
-    if (xhr.status === 200) {
-      callback(xhr.responseText);
-    } else if (xhr.status !== 200) {
-      errorCallback(xhr.responseText);
-    }
-  };
-  xhr.send(encodeURI(data));
-}
-
-if (commentForm) {
-  commentForm.addEventListener('submit', function (e) {
-    e.preventDefault();
-    sendFailedDiv.style.display = 'none';
-    sendSucceededDiv.style.display = 'none';
-    
-    var params = [];
-    
-    for (var i = 0; i < commentFormInputs.length; i++) {
-      var input = commentFormInputs[i];
-      params.push(input.name + '=' + input.value);
-    }
-    
-    overlayDiv.style.display = 'flex';
-    
-    post(e.target.getAttribute('data-action'), params.join('&'), function (text) {
-      commentForm.reset();
-      sendSucceededDiv.style.display = 'block';
-      overlayDiv.style.display = 'none';
-    }, function (text) {
-      sendFailedDiv.style.display = 'block';
-      overlayDiv.style.display = 'none';
-    });
-  });
-}
