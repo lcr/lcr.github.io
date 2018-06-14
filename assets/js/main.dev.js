870591e6fd20616530959da4fa516ec45fdb2b56
Leslie Lai <lcrc@foxmail.com>
2018-06-14 11:32:29 +0800

    commit

--- assets/js/main.dev.js
+++ assets/js/main.dev.js
@@ -128,6 +128,55 @@ if (urlHash.length > 0) {
   inputCheck.checked = true;
 }
 
+/*
+ * Comment Form
+ */
+var commentForm = document.querySelector('.comment-form');
+var commentFormInputs = document.querySelectorAll('.comment-form-input');
+var sendFailedDiv = document.querySelector('.comment-form-sendFailed');
+var sendSucceededDiv = document.querySelector('.comment-form-sendSucceeded');
+var overlayDiv = document.querySelector('.comment-form-overlay');
+
+function post(url, data, callback, errorCallback) {
+  var xhr = new XMLHttpRequest();
+  xhr.open('POST', url);
+  xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+  xhr.onload = function () {
+    if (xhr.status === 200) {
+      callback(xhr.responseText);
+    } else if (xhr.status !== 200) {
+      errorCallback(xhr.responseText);
+    }
+  };
+  xhr.send(encodeURI(data));
+}
+
+if (commentForm) {
+  commentForm.addEventListener('submit', function (e) {
+    e.preventDefault();
+    sendFailedDiv.style.display = 'none';
+    sendSucceededDiv.style.display = 'none';
+    
+    var params = [];
+    
+    for (var i = 0; i < commentFormInputs.length; i++) {
+      var input = commentFormInputs[i];
+      params.push(input.name + '=' + input.value);
+    }
+    
+    overlayDiv.style.display = 'flex';
+    
+    post(e.target.getAttribute('data-action'), params.join('&'), function (text) {
+      commentForm.reset();
+      sendSucceededDiv.style.display = 'block';
+      overlayDiv.style.display = 'none';
+    }, function (text) {
+      sendFailedDiv.style.display = 'block';
+      overlayDiv.style.display = 'none';
+    });
+  });
+}
+
 
 /*
  * google analytics
@@ -136,5 +185,5 @@ window.ga = window.ga || function () {
   (ga.q = ga.q || []).push(arguments)
 };
 ga.l = +new Date;
-ga('create', '{{ site.google_analytics }}', 'auto');
+ga('create', 'UA-107157614-1', 'auto');
 ga('send', 'pageview');
